9a56d02c7edcf44f31f12b73391e352a5332e53d
wtq <1906272972@qq.com>
2016-03-07 22:12:19 +0800

    add 344_IncreasingTripleSubsequences.py

---
+++ code/344_IncreasingTripleSubsequences.py
@@ -0,0 +1,17 @@
+# !/user/bin/env python
+# -*- coding: utf-8
+def increasingTriplet(nums):
+	len = nums.__len__()
+	if len < 3:
+		return False
+	second_min = -1
+	minnum = nums[0]
+	
+	for i in range(1, len):
+		if nums[i] <= minnum:
+			minnum = nums[i]
+		elif second_min == -1 or second_min >= nums[i]:
+			second_min = nums[i]
+		else:
+			return True
+q	return False
